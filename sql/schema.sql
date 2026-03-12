9e69cbbadeb58f966a002fac15856ddb1ca5c487
shalini2221 <sgauravtanpure2017@gmail.com>
2026-03-12 14:31:53 +0530

    Added database schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,32 @@
 CREATE TABLE companies (
 company_id SERIAL PRIMARY KEY,
-company_name VARCHAR(255),
-industry VARCHAR(100),
-country VARCHAR(100),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+company_name TEXT,
+industry TEXT,
+country TEXT,
+
+);
+
+CREATE TABLE activities (
+    activity_id SERIAL PRIMARY KEY,
+    company_id INT,
+    activity_type TEXT,
+    activity_value FLOAT,
+    unit TEXT,
+    activit_date DATE
+);
+
+CREATE TABLE emission_factors (
+factor_id SERIAL PRIMARY KEY,
+activity_type TEXT,
+emission_factor FLOAT,
+unit INT,
+source TEXT
+);
+
+CREATE TABLE emissions (
+emission_id SERIAL PRIMARY KEY,
+activity_id INT REFERENCES activities(activity_id),
+factor_id INT REFERENCES emission_factors(factor_id),
+calculated_emission FLOAT,
+calculation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
